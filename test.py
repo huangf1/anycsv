f3438af5f1c411563e7ab2c5e57ddb4efbf0c314
Freddy Huang <huangf1@rose-hulman.edu>
2019-05-15 22:26:29 -0400

    Test

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import anycsv
 import csv
 
-#reader = anycsv.reader(filename="data.csv")
-reader = anycsv.reader(url="https://dev.inpher.io/datasets/correlation/test1/bank-full-X.csv")
+reader = anycsv.reader(filename="data.csv")
+# reader = anycsv.reader(url="https://dev.inpher.io/datasets/correlation/test1/bank-full-X.csv")
 
-with open('testfile.csv', 'w') as f:
-    writer = csv.writer(f, delimiter='|')
+with open('result.csv', 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
     writer.writerows([row for row in reader])
